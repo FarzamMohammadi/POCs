961a6b1ff2c83fe43a25629621d590bb7ab451a6
Farzam Mohammadi <72108716+FarzamMohammadi@users.noreply.github.com>
2023-09-08 07:39:23 -0400

    Add Azure Service Bus DLQ purger

---
+++ DeadLetterPurge/.idea/.idea.DeadLetterPurge/.idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,3 @@
+
+J
+DeadLetterPurge/Program.cs,e\b\eb2d62d80942cffb2b3fee33972de20bd702c807
---
+++ DeadLetterPurge/DeadLetterPurge/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Azure.Messaging.ServiceBus;
+using Azure.Messaging.ServiceBus.Administration;
+
+namespace DeadLetterPurge;
+
+internal static class Program
+{
+    public static async Task Main(string[] args)
+    {
+        const string connectionString = "";
+
+        var serviceBusClient = new ServiceBusClient(connectionString);
+        var serviceBusAdministrationClient = new ServiceBusAdministrationClient(connectionString);
+
+        var queues = serviceBusAdministrationClient.GetQueuesAsync();
+
+        var receiverOptions = new ServiceBusReceiverOptions
+        {
+            ReceiveMode = ServiceBusReceiveMode.ReceiveAndDelete,
+            SubQueue = SubQueue.DeadLetter
+        };
+
+        await foreach (var queue in queues)
+        {
+            var queueName = queue.Name;
+
+            Console.WriteLine("Purging dead-letters. Queue: " + queueName);
+
+            var receiver = serviceBusClient.CreateReceiver(queueName, receiverOptions);
+
+            var maxTimeToWaitForNextMessage = TimeSpan.FromSeconds(5);
+            
+            try
+            {
+                IReadOnlyList<ServiceBusReceivedMessage> serviceBusReceivedMessages;
+
+                do
+                {
+                    serviceBusReceivedMessages = await receiver.ReceiveMessagesAsync(5000, maxTimeToWaitForNextMessage)
+                        .ConfigureAwait(false);
+                } while (serviceBusReceivedMessages?.Any() == true); // This list is said to never return null, but taking an extra precaution here.
+
+                Console.WriteLine("Dead-letter purge complete. Queue: " + queueName);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Dead-letter purge failed. Queue: " + queueName + " " + ex);
+            }
+        }
+
+        Console.WriteLine("Dead-letter purge job complete. Next scheduled run will be in 3 days");
+    }
+}
